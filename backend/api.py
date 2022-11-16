a7706f5a1b0ca015a708f09d36e75031d70bd815
denyskononenko <kononenkody@gmail.com>
2022-11-17 00:56:18 +0100

    fix: test lattice is added

--- backend/api.py
+++ backend/api.py
@@ -1,6 +1,7 @@
 import os
 import time
 from flask import Flask, request
+from pymatgen.core.structure import Structure  
 
 app = Flask(__name__)
 
@@ -11,8 +12,11 @@ def get_spin_model():
     f = request.files['file']
     f.save(UPLOAD_PATH)
 
+    structure = Structure.from_file(UPLOAD_PATH)
+    lattice = structure.lattice.matrix.tolist()
+
     response = {
-        "lattice"  : [[1,0,0], [0,1,0], [0,0,1]],
+        "lattice"  : lattice,
         "exchanges": [0.5, 0.05],
         "vertices" : [[2, 2, 0], [2, 3, 0], [2, 4, 0], [2, 2, 1], [2, 3, 1], [2, 4, 1]],
         "edges"    : [[[0, 1], [1, 2], [3, 4], [4, 5]], [[0, 3], [1, 4], [2, 5]]]
