8439911090970926a6f46950c1f00648e1cebb6b
denyskononenko <kononenkody@gmail.com>
2022-11-17 01:12:57 +0100

    feat: auxilary file with declaration of constants is added

---
+++ src/declarations.js
@@ -0,0 +1,10 @@
+// colors for marking different transfer integrals 
+const ExchangeColors = [
+    "#f4e604",
+    "#a7e237",
+    "#37bd79", 
+    "#308fac",
+    "#0457ac"
+];
+
+export default ExchangeColors;
