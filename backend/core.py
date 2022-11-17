5cb5f6be74cb92cee6fdf90a63a6558ba2698fce
denyskononenko <kononenkody@gmail.com>
2022-11-17 17:34:47 +0100

    feat: class for SpinModel is added

--- backend/core.py
+++ backend/core.py
@@ -130,6 +130,140 @@ class CrystalEnvironment:
 
         return False
 
+class SpinModel:
+    def __init__(self, 
+                    structure: Structure, 
+                    basis, 
+                    scaler_X, 
+                    scaler_Y, 
+                    feat_selector, 
+                    model,
+                    r_cu_cu_max: float = 6.0, 
+                    scaling: list = [2, 2, 2],
+                    nmax: int = _N_MAX_
+                    ):
+        self.structure = structure
+        self.basis  = basis 
+        self.scaler_X =  scaler_X
+        self.scaler_Y = scaler_Y
+        self.feat_selector = feat_selector
+        self.model = model
+        self.r_cu_cu_max = r_cu_cu_max
+        self.scaling = scaling         # scaling coefficients to build the super cell
+        self.nmax = nmax               # maximum n for basis of Zernike 3D functions
+        self.cu_envs, self.cu_couplings = self.struct2env()
+
+    @property 
+    def superstructure(self):
+        """Return structure with scaled unit cell."""
+        superstructure = self.structure.copy()
+        superstructure.make_supercell(scaling_matrix=self.scaling) # make the supercell
+        return superstructure
+
+    @property
+    def cu_sites(self):
+        """Select Cu2+ stes from the upscaled unit cell."""
+        cu_sites = [] # list to store Cu2+ sites        
+                
+        # find Cu2+ sites 
+        for site in self.superstructure:
+            if get_site_Z(site) == 29 and get_site_OxN(site) == 2:
+                cu_sites.append(site)
+        print(type(cu_sites[1]))
+        return cu_sites
+
+    @property
+    def vertices(self):
+        """Vertices of the spin model as [[x,y,z], ... ]"""
+        return [site.coords.tolist() for site in self.cu_sites]
+
+    @property
+    def edges(self):
+        """Edges of the spin model."""
+        return self.cu_couplings
+    
+    @property
+    def exchanges(self):
+        return [self.descr2hop(self.env2descr(env)) for env in self.cu_envs]
+    
+    @property
+    def distances(self):
+        return [round(env.cu_cu_dist, 3) for env in self.cu_envs]
+
+    @timeit
+    def struct2env(self) -> list:
+        """ 
+        Select all unique Cu-Cu crystal environments for given structure.
+        
+        Return:
+            (list[CrystalEnvironment], [[(i,j), ...]]) list of crystal environments and couplings
+        """
+        # make supercell 
+        cu_envs = []                      # list for Cu2+ pairs environments
+        cu_couplings = []                 # indices of coupled Cu2+ sites for each determined hopping
+        cu_pairs_count = 0                # counter of Cu2+ pairs 
+
+        # find Cu2+ pairs 
+        for i in range(len(self.cu_sites) - 1):
+            for j in range(i + 1, len(self.cu_sites)):
+                cu_pairs_count += 1
+                cu1, cu2 = self.cu_sites[i], self.cu_sites[j]
+                t_env = CrystalEnvironment(self.superstructure, cu1, cu2)
+                if (t_env.cu_cu_dist <= self.r_cu_cu_max):
+                    if t_env not in cu_envs:
+                        cu_envs.append(t_env)
+                        cu_couplings.append([(i, j)])
+                    else:
+                        for l, e in enumerate(cu_envs):
+                            if t_env == e:
+                                cu_couplings[l].append((i, j))
+        
+        print(f'Sites in the cell: {len(self.structure)}')
+        print(f'Sites in the supercell: {len(self.superstructure)}')
+        print(f'# of Cu sites in the supercell: {len(self.cu_sites)}')
+        print(f'# of pairs: {cu_pairs_count}')
+        print(f'# of unique environemnts: {len(cu_envs)}')
+        print(f'Couplings in the supercell:')
+        for _ in cu_couplings:
+            print(_)
+
+        return cu_envs, cu_couplings
+    
+    def env2descr(self, env: CrystalEnvironment) -> list:
+        """
+        Create rotationally-invariant 3D Zernike desriptor for local crystal environment.
+        Args:
+            env: (CrystalEnvironment) local crystal environment for which to create descriptor
+        Return: 
+            (dict) rotationally-invariant 3D Zernike descriptor
+        """
+        dist = env.cu_cu_dist
+        env_atoms = env.get_atoms()
+        env_atoms = np.array(env_atoms, dtype=np.float32)
+        # convert atoms in environment to the 3D image 
+        img3d = make_3d_img(env_atoms, ds=0.025)
+        # make rotationally invariant Zernike 3D descriptor 
+        descr = zernike3d_descriptor_invariant(img3d, self.basis, self.nmax)  # [[n, l, Coeff], ...]
+
+        X = np.array([[dist] + [_[2] for _ in descr]])
+        print(f'Predictors: {len(X)}')
+        return X
+    
+    def descr2hop(self, descr: list):
+        """Predict the hopping for the given descriptor of the local crystal environment."""
+        # min max scale the feature vector 
+        X = self.scaler_X.transform(descr)
+        # select important features 
+        X_new = self.feat_selector.transform(X)
+        # predict the hopping, value is scaled from 0 to 1
+        Y_pred = self.model.predict(X_new) 
+        # inverse transform the predicted value 
+        Y_pred = self.scaler_Y.inverse_transform(Y_pred.reshape(-1, 1))
+        return Y_pred.flatten()[0]
+
+def validate_cif(cif_file: str):
+    pass
+
 @timeit
 def struct2env(structure: Structure, r_max: float) -> list:
     """ 
@@ -224,20 +358,26 @@ if __name__ == "__main__":
     cif_path = '/home/denys/Documents/ifw/smc/data/raw/531.cif'
 
     struct = Structure.from_file(cif_path)
-    envs = struct2env(struct, 6.0)
-    test_env = envs[1]
-    print(envs[0] == envs[0])
-    print(envs[0] == envs[1])
-    
+    scaler_X, scaler_Y, feat_selector, loaded_model = load_ml()
     basis_size = calc_basis_size(_N_MAX_)
     basis = load_basis(basis_path, basis_size)
 
-    test_descr = env2descr(test_env, basis)
-    pred_t = descr2hop(test_descr)
+    test_spin_model = SpinModel(struct, basis, scaler_X, scaler_Y, feat_selector, loaded_model)
 
-    print(f'Test env dist: {test_env.cu_cu_dist} Ang')
-    print(f'Pred t = {pred_t} eV')
+    print(test_spin_model.distances)
+    print(test_spin_model.exchanges)
+    print(test_spin_model.edges)
+    print(test_spin_model.edges)
+
+    #envs = struct2env(struct, 6.0)
+    #test_env = envs[0]
+    #print(envs[0] == envs[0])
+    #print(envs[0] == envs[1])
     
-    #for env in envs:
-    #    print(env['dist'], env['env'].cu_cu_dist, len(env['env'].get_atoms()))
+    #test_descr = env2descr(test_env, basis)
+    #pred_t = descr2hop(test_descr)
+
+    #print(f'Test env dist: {test_env.cu_cu_dist} Ang')
+    #print(f'Pred t = {pred_t} eV')
+    
     
