99612e76277ccfc9673fb23d76f0c712900bf373
denyskononenko <kononenkody@gmail.com>
2022-11-25 15:51:03 +0100

    fix: extension of the list with colors for exchanges

--- src/declarations.js
+++ src/declarations.js
@@ -1,10 +1,24 @@
 // colors for marking different transfer integrals 
 const ExchangeColors = [
+    "#de3163",
+    "#FF7F50",
     "#f4e604",
     "#a7e237",
     "#37bd79", 
     "#308fac",
-    "#0457ac"
+    "#0457ac",
+    "#000080",
+    "#800080",
+    "#C0C0C0",
+    "#e6c5bf",
+    "#c2a594",
+    "#93a197",
+    "#6c7a85",
+    "#4faea5",
+    "#4cb7ad",
+    "#4fa59d",
+    "#5db7ae",
+    "#3e968e"
 ];
 
 export default ExchangeColors;
