fa2d552ff283123d8af094c57443b181be647198
denyskononenko <kononenkody@gmail.com>
2023-08-02 17:24:44 +0200

    fix: app version is added in the header

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
@@ -8,7 +9,7 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import DescriptionIcon from '@mui/icons-material/Description';
 import AppLogo from './logo.svg';
 
-const linkPaper = "https://arxiv.org/";
+const linkPaper = "https://arxiv.org/abs/2303.14033";
 const linkCode  = "https://github.com/denyskononenko/smcp";
 
 class Form extends Component {
@@ -26,13 +27,21 @@ class Form extends Component {
                     sx={{ mr: 2 }}>
                     <img src={AppLogo} alt="Logo" />
                 </IconButton>
-        
-                <Typography variant="h6" 
-                    component="div" 
-                    color="#FFFFFF"
-                    sx={{ flexGrow: 1 }}>
-                    Spin Models for Cuprates Predictor
-                </Typography>
+
+                <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                    <Typography variant="h6" 
+                        component="div" 
+                        color="#FFFFFF"
+                        sx={{ }}>
+                        Spin Models for Cuprates Predictor
+                    </Typography>
+                    <Typography variant="subtitle1" 
+                        component="div" 
+                        color="#FFFFFF"
+                        sx={{ marginLeft: 1 }}>
+                        v0.1
+                    </Typography>
+                </Box>
                 <a href={linkPaper} target="_blank" rel="noreferrer">
                     <Button 
                         endIcon={<DescriptionIcon />}
